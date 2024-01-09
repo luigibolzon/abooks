c6c9c5a9d12ffb450a75198d290484e0e00d74ca
Luigi <luigibolzon@gmail.com>
2024-01-09 15:33:44 -0300

    já tem a parte superior, mas precisa simplificador com componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import Logo from './componentes/Logo'
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+
+const textoOpcoes = ['Categorias', 'Minha estante', 'Favoritos']
+const icones = [perfil, sacola]
 
 function App() {
   return (
-    <div>
-      <header >
-        <div >
+    <div className='App'>
+      <header className='App-header'>
+        <Logo></Logo>
+        <ul className='opcoes'>
+          {textoOpcoes.map((texto) => (<li className='opcao'><p>{texto}</p></li>))}
+        </ul>
 
-        </div>
-
+        <ul className='icones'>
+          {icones.map((icone) => (
+            <li className='icone'><img src={icone}></img></li>))}
+        </ul> 
       </header>
-    </div>
+    </div >
   );
 }
 
 export default App;
-  
